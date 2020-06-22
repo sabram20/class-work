29bc5f090c362a05a18b21c81d56a521b48ad059
sabram <66972158+sabram20@users.noreply.github.com>
2020-06-22 14:50:43 -0500

    Add files via upload

--- Student.py
+++ Student.py
@@ -4,3 +4,7 @@ class Student:
         self.major = major
         self.gpa = gpa
         self.is_on_probation = is_on_probation
+
+student1 = Student("Mike Clark", "Electrical Engineering", 4.0, False)
+
+print(student1.name)
